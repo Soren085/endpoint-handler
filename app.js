f74862e7de2f48868dea73f68dad921a3a676e19
Soren085 <giangtuan085@gmail.com>
2020-04-21 13:57:23 +0700

    Refactor structure

---
+++ app.js
@@ -0,0 +1,60 @@
+const bodyParser = require('body-parser');
+
+// fire function
+
+
+// create application/json parser
+const jsonParser = bodyParser.json();
+ 
+// create application/x-www-form-urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+ 
+// POST /login gets urlencoded bodies
+express.post('/login', urlencodedParser, function (req, res) {
+  res.send('welcome, ' + req.body.username);
+})
+ 
+// POST /api/users gets JSON bodies
+express.post('/api/users', jsonParser, function (req, res) {
+	// create user in req.body
+	console.log('/api/users, requested');
+})
+
+// handle request
+express.get('/', (req, res) => {
+	res.sendFile(__dirname + '/index.html');
+});
+
+// middleware
+// express.use('/assets', (req, res, next) => {
+// });
+
+express.use('/assets', expModule.static('assets'));
+
+express.get('/cat', (req, res) => {
+	res.send('cat page');
+	res.sendStatus(200);
+});
+
+express.get('/cat/:name', (req, res) => {
+	res.send(`cat name is : ${req.params.name}`);
+});
+
+
+// const serveContent = (res, json = false, contentType, file = '', encoding = 'utf8') => {
+// 	res.writeHead(200, {'Content-type': contentType});
+// 	if (json) {
+// 		const obj = {
+// 			name: 'cat',
+// 			age: 12
+// 		};
+// 		res.end(JSON.stringify(obj));
+// 	} else {
+// 		const readStream = fs.createReadStream(__dirname + file, encoding);
+// 		readStream.pipe(res);
+// 	}
+// };
+
+// PORT for Heroku host or default PORT
+express.listen(process.env.PORT || 3000);
+// console.log('Now listening to port 3k');
---
+++ db/index.js
@@ -0,0 +1,7 @@
+const expModule = require('express');
+const express = expModule();
+
+const cors = require('cors');
+
+// CORS allow
+express.use(cors());
